8a72718e581c4591b476b4726e2a34439f9337ec
Simon-Sahlin <simonsahlin@somin.se>
2024-02-11 20:36:40 +0100

    Add Custom Grid Generation Functionality

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-let rowCount = 100;
-let columnCount = 100;
-
+let submitButton = document.querySelector("#submitButton");
 let gridWrapper = document.querySelector("#gridWrapper")
 
-for (let y = 0; y < rowCount; y++){
-    let rowWrapper = document.createElement("div");
-    rowWrapper.classList.add("rowWrapper");
-
-    for (let x = 0; x < columnCount; x++){
-        let div = document.createElement("div");
-        div.classList.add("gridCell");
-
-        div.addEventListener("mouseenter", () =>{
-            div.style.backgroundColor = "black";
-        });
-
-        rowWrapper.appendChild(div);
+submitButton.addEventListener("click", ()=>{
+    while (gridWrapper.firstChild){
+        gridWrapper.removeChild(gridWrapper.firstChild);
     }
-    gridWrapper.appendChild(rowWrapper);
+    let gridSize = +prompt("Grid Size:");
+    if (gridSize > 200){
+        alert("Size of grid can be a maximum of 200!");
+        return;
+    }
+    generateGrid(gridSize);
+}
+)
+
+function generateGrid(size = 90){
+    for (let y = 0; y < size; y++){
+        let rowWrapper = document.createElement("div");
+        rowWrapper.classList.add("rowWrapper");
+    
+        for (let x = 0; x < size; x++){
+            let div = document.createElement("div");
+            div.classList.add("gridCell");
+            div.style.width = `${900/size}px`;
+            div.style.height = `${900/size}px`;
+    
+            div.addEventListener("mouseenter", () =>{
+                div.style.backgroundColor = "black";
+                // div.style.backgroundColor = '#'+ Math.floor(Math.random() * 19777215).toString(16);
+            });
+    
+            rowWrapper.appendChild(div);
+        }
+        gridWrapper.appendChild(rowWrapper);
+    }
 }
